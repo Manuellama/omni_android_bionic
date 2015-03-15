ba894e93fa40a40d945c2a7d1053db49717152c6
Daniel Tjandra <danieltjandra@gmail.com>
2015-03-15 13:34:36 -0400

    mooch stpcpy assembly from a15 for tegra2

--- libc/arch-arm/tegra2/tegra2.mk
+++ libc/arch-arm/tegra2/tegra2.mk
@@ -6,7 +6,8 @@ libc_bionic_src_files_arm += \
 	arch-arm/generic/bionic/strcpy.S \
 	bionic/__strcat_chk.cpp \
 	bionic/__strcpy_chk.cpp \
-	bionic/memmove.c
+	bionic/memmove.c \
+    arch-arm/cortex-a15/bionic/stpcpy.S \
 
 # armv7-a (non neon) ones from cm10.1
 libc_bionic_src_files_arm += \
